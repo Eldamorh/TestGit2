31a3d86c74ba32aa32a2271ed60f59e90446fe41
Eldus <arsik2001@gmail.com>
2020-12-16 21:26:28 +0200

    Plus method

--- src/com/eldus/Calculator.java
+++ src/com/eldus/Calculator.java
@@ -2,9 +2,11 @@ package com.eldus;
 
 public class Calculator {
 
-    public static int plus(int a, int b){
-
-
-        return 0;
+    public static int plus(int[] arr) {
+        int sum = 0;
+        for (int a : arr) {
+            sum += a;
+        }
+        return sum;
     }
 }
